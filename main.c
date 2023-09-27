9a009d51bf9d26180715220e85995429cc4ea5ac
Ansem717 <me@andymalik.com>
2023-09-27 01:35:48 -0700

    Implemented building blocks to run four games at the same time.

--- main.c
+++ main.c
@@ -13,57 +13,148 @@
 //---------------------------------------------------------
 
 #include "cprocessing.h"
+#include "main.h"
+#define DEBUG 0
 
 float WIDTH, HEIGHT;
-CP_Color BLACK, GRAY, WHITE, RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA;
+CP_Color BLACK, GRAY, LIGHT_GRAY, WHITE, RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA;
+
+typedef struct {
+	CP_KEY keyLeft;
+	CP_KEY keyRight;
+} Controller;
+
+typedef struct {
+	int id;
+	Controller cont;
+	CP_Color col;
+	CP_BOOL active;
+	FunctionPtr play;
+} Game;
+
+Controller CONTROLLERS[4];
+Game GAMES[4];
+
+float margin, padding;
+
+void fill(CP_Color c) {
+	CP_Settings_Fill(c);
+}
+
+void stroke(CP_Color c, float w) {
+	CP_Settings_Stroke(c);
+	CP_Settings_StrokeWeight(w);
+}
 
 void settingsInit(void) {
+#if DEBUG
+	CP_System_SetWindowSize(900, 900);
+#else
 	CP_System_Fullscreen();
+#endif
 
-	WIDTH = CP_System_GetWindowWidth();
+	WIDTH = CP_System_GetWindowWidth() - margin * 2;
 	HEIGHT = CP_System_GetWindowHeight();
 
 	CP_Settings_EllipseMode(CP_POSITION_CENTER);
 	CP_Settings_ImageMode(CP_POSITION_CENTER);
-	CP_Settings_RectMode(CP_POSITION_CENTER);
+	CP_Settings_RectMode(CP_POSITION_CORNER);
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
 
-	BLACK = CP_Color_Create(0, 0, 0, 255);
-	GRAY = CP_Color_Create(120, 120, 120, 255);
-	WHITE = CP_Color_Create(255, 255, 255, 255);
-	RED = CP_Color_Create(255, 0, 0, 255);
-	YELLOW = CP_Color_Create(255, 255, 0, 255);
-	GREEN = CP_Color_Create(0, 255, 0, 255);
-	CYAN = CP_Color_Create(0, 255, 255, 255);
-	BLUE = CP_Color_Create(0, 0, 255, 255);
-	MAGENTA = CP_Color_Create(255, 0, 255, 255);
-
-	CP_Settings_Fill(WHITE);
-	CP_Settings_Stroke(BLACK);
-	CP_Settings_StrokeWeight(10);
+	fill(WHITE);
+	stroke(BLACK, 2);
 	CP_Settings_TextSize(40.0f);
-	CP_Settings_NoTint();
 
 	CP_Settings_Save();
 }
 
+void variablesInit(void) {
+	margin = 100.0f;
+	padding = 40.0f;
+
+	BLACK = CP_Color_Create(0, 0, 0, 255);
+	GRAY = CP_Color_Create(70, 70, 70, 255);
+	LIGHT_GRAY = CP_Color_Create(180, 180, 180, 255);
+	WHITE = CP_Color_Create(255, 255, 255, 255);
+	RED = CP_Color_Create(70, 0, 0, 255);
+	YELLOW = CP_Color_Create(70, 70, 0, 255);
+	GREEN = CP_Color_Create(0, 70, 0, 255);
+	CYAN = CP_Color_Create(0, 70, 70, 255);
+	BLUE = CP_Color_Create(0, 0, 70, 255);
+	MAGENTA = CP_Color_Create(70, 0, 70, 255);
+
+	CONTROLLERS[0].keyLeft = KEY_A;
+	CONTROLLERS[0].keyRight = KEY_S;
+	CONTROLLERS[1].keyLeft = KEY_D;
+	CONTROLLERS[1].keyRight = KEY_F;
+	CONTROLLERS[2].keyLeft = KEY_J;
+	CONTROLLERS[2].keyRight = KEY_K;
+	CONTROLLERS[3].keyLeft = KEY_L;
+	CONTROLLERS[3].keyRight = KEY_SEMICOLON;
+
+	GAMES[0].id = 0;
+	GAMES[0].cont = CONTROLLERS[0];
+	GAMES[0].col = RED;
+	GAMES[0].active = FALSE;
+	GAMES[0].play = &laneDriver;
+
+	GAMES[1].id = 1;
+	GAMES[1].cont = CONTROLLERS[1];
+	GAMES[1].col = GREEN;
+	GAMES[1].active = TRUE;
+	GAMES[1].play = &pinball;
+
+	GAMES[2].id = 2;
+	GAMES[2].cont = CONTROLLERS[2];
+	GAMES[2].col = BLUE;
+	GAMES[2].active = FALSE;
+	GAMES[2].play = &tronRacing;
+
+	GAMES[3].id = 3;
+	GAMES[3].cont = CONTROLLERS[3];
+	GAMES[3].col = YELLOW;
+	GAMES[3].active = FALSE;
+	GAMES[3].play = &raftCollector;
+
+}
+
 void gameInit(void) {
+	variablesInit();
 	settingsInit();
 }  
 
 void gameUpdate(void) { 
 	CP_Graphics_ClearBackground(BLACK);
+	CP_Settings_Translate(margin, 0);
 
-	if (CP_Input_KeyReleased(KEY_R)) CP_Settings_Tint(RED);
-	if (CP_Input_KeyReleased(KEY_G)) CP_Settings_Tint(GREEN);
-	if (CP_Input_KeyReleased(KEY_B)) CP_Settings_Tint(BLUE);
-	if (CP_Input_KeyReleased(KEY_C)) CP_Settings_Tint(CYAN);
-	if (CP_Input_KeyReleased(KEY_Y)) CP_Settings_Tint(YELLOW);
-	if (CP_Input_KeyReleased(KEY_M)) CP_Settings_Tint(MAGENTA);
+	float gameWidth = (WIDTH - padding * 3) / 4;
+	float xStep = gameWidth + padding;
 
-	CP_Settings_Fill(GRAY);
+	for (int i = 0; i < 4; i++) {
+		//for each GAME g
+		Game g = GAMES[i];
+		if (g.active) {
+			fill(LIGHT_GRAY);
+			stroke(g.col, padding);
+			CP_Graphics_DrawRect(xStep * g.id, 0, gameWidth, HEIGHT);
+			g.play();
+		}
+	}
+}
 
-	CP_Graphics_DrawRect(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 2);
+void laneDriver() {
+
+}
+
+void pinball() {
+	CP_Graphics_DrawCircle(500, 500, 100);
+}
+
+void tronRacing() {
+
+}
+
+void raftCollector() {
 
 }
 
---
+++ main.h
@@ -0,0 +1,4 @@
+void laneDriver(void);
+void pinball(void);
+void tronRacing(void);
+void raftCollector(void);
