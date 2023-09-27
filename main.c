7803957586fa8de9706c160ca2139ed78f747a1d
Ansem717 <me@andymalik.com>
2023-09-27 05:39:03 -0700

    Not sure how to use structs and pointers exactly. I also hope I didn't mess up my commit.

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include "cprocessing.h"
 #include "main.h"
 #include <math.h>
-#define DEBUG 0
+#define DEBUG 1
 
 float WIDTH, HEIGHT;
 CP_Color BLACK, GRAY, LIGHT_GRAY, WHITE, RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA;
@@ -34,6 +34,7 @@ typedef struct {
 	Controller cont;
 	CP_Color col;
 	CP_BOOL active;
+	CP_BOOL init;
 	void (*play)(Game);
 } Game;
 
@@ -42,10 +43,13 @@ Game GAMES[4];
 
 float margin, padding;
 
+CP_Vector bouncerCenter[4];
+float bouncerSize;
 float paddleLTheta, paddleRTheta, paddleMaxTheta, paddleMinTheta;
 CP_BOOL paddleLTriggered, paddleRTriggered;
 float paddleAccSpeed, paddleDecSpeed;
-
+CP_Vector pinballPos, pinballVel;
+float gravity;
 
 void fill(CP_Color c) {
 	CP_Settings_Fill(c);
@@ -112,7 +116,8 @@ void variablesInit(void) {
 	GAMES[0].id = 0;
 	GAMES[0].cont = CONTROLLERS[0];
 	GAMES[0].col = RED;
-	GAMES[0].active = TRUE;
+	GAMES[0].active = FALSE;
+	GAMES[0].init = FALSE;
 	GAMES[0].play = &breakout;
 
 	/***************\
@@ -122,6 +127,7 @@ void variablesInit(void) {
 	GAMES[1].cont = CONTROLLERS[1];
 	GAMES[1].col = GREEN;
 	GAMES[1].active = TRUE;
+	GAMES[1].init = FALSE;
 	GAMES[1].play = &pinball;
 
 	paddleLTheta = 120;
@@ -130,7 +136,11 @@ void variablesInit(void) {
 	paddleMinTheta = 60;
 	paddleLTriggered = paddleRTriggered = FALSE;
 	paddleAccSpeed = 16; 
-	paddleDecSpeed = 4;
+	paddleDecSpeed = 5;
+
+	gravity = -1;
+	pinballPos = CP_Vector_Zero();
+	pinballVel = CP_Vector_Set(0, gravity);
 
 	/*******************\
 	| LANE DRIVER INITS |
@@ -138,7 +148,8 @@ void variablesInit(void) {
 	GAMES[2].id = 2;
 	GAMES[2].cont = CONTROLLERS[2];
 	GAMES[2].col = BLUE;
-	GAMES[2].active = 1;
+	GAMES[2].active = FALSE;
+	GAMES[2].init = FALSE;
 	GAMES[2].play = &laneDriver;
 
 	/**********************\
@@ -147,7 +158,8 @@ void variablesInit(void) {
 	GAMES[3].id = 3;
 	GAMES[3].cont = CONTROLLERS[3];
 	GAMES[3].col = YELLOW;
-	GAMES[3].active = 1;
+	GAMES[3].active = FALSE;
+	GAMES[3].init = FALSE;
 	GAMES[3].play = &raftCollector;
 
 }
@@ -175,7 +187,7 @@ void gameUpdate(void) {
 			g.w = gameWidth;
 			g.h = HEIGHT - padding;
 			CP_Graphics_DrawRect(g.x, g.y, g.w, g.h);
-			g.play(g);
+			g.play(&g);
 		}
 	}
 }
@@ -184,46 +196,75 @@ void breakout(Game g) {
 
 }
 
-void pinball(Game g) {
-	stroke(BLACK, 4);
-	fill(g.col);
-	//BOUNCERS
-	float bouncerSize = 80;
-	CP_Graphics_DrawCircle(g.x + g.w * 1 / 4, g.y + g.h * 4 / 16, bouncerSize);
-	CP_Graphics_DrawCircle(g.x + g.w * 3 / 4, g.y + g.h * 4 / 16, bouncerSize);
-	CP_Graphics_DrawCircle(g.x + g.w * 2 / 4, g.y + g.h * 7 / 16, bouncerSize);
+void pinball(Game* g) {
+	if (g->init == FALSE) {
+		//BOUNCERS
+		bouncerSize = 80;
+		bouncerCenter[0].x = g->x + g->w * 1 / 4;
+		bouncerCenter[0].y = g->y + g->h * 6 / 32;
+		bouncerCenter[1].x = g->x + g->w * 3 / 4;
+		bouncerCenter[1].y = g->y + g->h * 6 / 32;
+		bouncerCenter[2].x = g->x + g->w * 2 / 4;
+		bouncerCenter[2].y = g->y + g->h * 12 / 32;
+		bouncerCenter[3].x = g->x + g->w * 8 / 16;
+		bouncerCenter[3].y = g->y + g->h * 77 / 128;
 
-	//PADDLES
-	stroke(BLACK, 40);
+		//PINBALL
+		pinballPos.x = g->x + g->w * 1 / 2;
+		pinballPos.y = g->y + g->h * 1 / 4;
 
-	if (paddleLTriggered) {
-		if (paddleLTheta > paddleMinTheta) { paddleLTheta -= paddleAccSpeed; } else { paddleLTriggered = FALSE; }
-	} else if (paddleLTheta < paddleMaxTheta) paddleLTheta += paddleDecSpeed;
+		g->init = TRUE;
+	} else {
+		//BOUNCERS
+		stroke(BLACK, 4);
+		fill(g->col);
+		for (int i = 0; i < 3; i++) {
+			CP_Graphics_DrawCircle(bouncerCenter[i].x, bouncerCenter[i].y, bouncerSize);
+		}
+		CP_Graphics_DrawTriangle(
+			g->x + g->w * 8 / 16,
+			g->y + g->h * 18 / 32,
+			g->x + g->w * 7 / 16,
+			g->y + g->h * 20 / 32,
+			g->x + g->w * 9 / 16,
+			g->y + g->h * 20 / 32
+		);
 
-	if (paddleRTriggered) {
-		if (paddleRTheta > paddleMinTheta) { paddleRTheta -= paddleAccSpeed; } else { paddleRTriggered = FALSE; }
-	} else if (paddleRTheta < paddleMaxTheta) paddleRTheta += paddleDecSpeed;
+		//PADDLES
+		stroke(BLACK, 40);
+		if (paddleLTriggered) {
+			if (paddleLTheta > paddleMinTheta) { paddleLTheta -= paddleAccSpeed; } else { paddleLTriggered = FALSE; }
+		} else if (paddleLTheta < paddleMaxTheta) paddleLTheta += paddleDecSpeed;
 
-	CP_Graphics_DrawLine(
-		g.x + padding / 2,
-		g.y + g.h * 12 / 16,
-		g.x + padding / 2 + (g.w * 3 / 8) * sin(CP_Math_Radians(paddleLTheta)),
-		g.y + g.h * 12 / 16 + (g.h * 2 / 16) * -cos(CP_Math_Radians(paddleLTheta))
-	);
+		if (paddleRTriggered) {
+			if (paddleRTheta > paddleMinTheta) { paddleRTheta -= paddleAccSpeed; } else { paddleRTriggered = FALSE; }
+		} else if (paddleRTheta < paddleMaxTheta) paddleRTheta += paddleDecSpeed;
 
-	CP_Graphics_DrawLine(
-		g.x + g.w - padding / 2,
-		g.y + g.h * 12 / 16,
-		g.x + g.w - padding / 2 - (g.w * 3 / 8) * sin(CP_Math_Radians(paddleRTheta)),
-		g.y + g.h * 12 / 16 + (g.h * 2 / 16) * -cos(CP_Math_Radians(paddleRTheta))
-	);
+		CP_Graphics_DrawLine(
+			g->x + padding / 2,
+			g->y + g->h * 12 / 16,
+			g->x + padding / 2 + (g->w * 3 / 8) * sin(CP_Math_Radians(paddleLTheta)),
+			g->y + g->h * 12 / 16 + (g->h * 2 / 16) * -cos(CP_Math_Radians(paddleLTheta))
+		);
 
-	stroke(BLACK, 2);
-	fill(GRAY);
-	//CP_Graphics_DrawCircle(g.x + g.w/2, g.y + g.h/2, 50);
+		CP_Graphics_DrawLine(
+			g->x + g->w - padding / 2,
+			g->y + g->h * 12 / 16,
+			g->x + g->w - padding / 2 - (g->w * 3 / 8) * sin(CP_Math_Radians(paddleRTheta)),
+			g->y + g->h * 12 / 16 + (g->h * 2 / 16) * -cos(CP_Math_Radians(paddleRTheta))
+		);
 
-	if (CP_Input_KeyTriggered(g.cont.keyLeft) && paddleLTheta >= paddleMaxTheta) paddleLTriggered = TRUE;
-	if (CP_Input_KeyTriggered(g.cont.keyRight) && paddleRTheta >= paddleMaxTheta) paddleRTriggered = TRUE;
+
+		//PINBALL
+		stroke(BLACK, 2);
+		fill(GRAY);
+		CP_Graphics_DrawCircle(pinballPos.x, pinballPos.y, 45);
+		pinballPos = CP_Vector_Add(pinballPos, pinballVel);
+
+
+		if (CP_Input_KeyTriggered(g->cont.keyLeft) && paddleLTheta >= paddleMaxTheta) paddleLTriggered = TRUE;
+		if (CP_Input_KeyTriggered(g->cont.keyRight) && paddleRTheta >= paddleMaxTheta) paddleRTriggered = TRUE;
+	}
 }
 
 void laneDriver(Game g) {
