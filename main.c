1eb868638aa3f8664469c694c49cd18b239f53c7
Ansem717 <me@andymalik.com>
2023-09-26 23:24:43 -0700

    A quick test on the Tint tool in CP.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 //---------------------------------------------------------
 // file:	main.c
-// author:	[NAME]
-// email:	[DIGIPEN EMAIL ADDRESS]
+// author:	Andy Malik
+// email:	andy.malik@digipen.edu
 //
 // brief:	Main entry point for the sample project
 //			of the CProcessing library
@@ -14,33 +14,71 @@
 
 #include "cprocessing.h"
 
-// use CP_Engine_SetNextGameState to specify this function as the initialization function
-// this function will be called once at the beginning of the program
-void game_init(void)
-{
-	// initialize variables and CProcessing settings for this gamestate
+float WIDTH, HEIGHT;
+CP_Color BLACK, GRAY, WHITE, RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA;
+
+void settingsInit(void) {
+	CP_System_Fullscreen();
+
+	WIDTH = CP_System_GetWindowWidth();
+	HEIGHT = CP_System_GetWindowHeight();
+
+	CP_Settings_EllipseMode(CP_POSITION_CENTER);
+	CP_Settings_ImageMode(CP_POSITION_CENTER);
+	CP_Settings_RectMode(CP_POSITION_CENTER);
+	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
+
+	BLACK = CP_Color_Create(0, 0, 0, 255);
+	GRAY = CP_Color_Create(120, 120, 120, 255);
+	WHITE = CP_Color_Create(255, 255, 255, 255);
+	RED = CP_Color_Create(255, 0, 0, 255);
+	YELLOW = CP_Color_Create(255, 255, 0, 255);
+	GREEN = CP_Color_Create(0, 255, 0, 255);
+	CYAN = CP_Color_Create(0, 255, 255, 255);
+	BLUE = CP_Color_Create(0, 0, 255, 255);
+	MAGENTA = CP_Color_Create(255, 0, 255, 255);
+
+	CP_Settings_Fill(WHITE);
+	CP_Settings_Stroke(BLACK);
+	CP_Settings_StrokeWeight(10);
+	CP_Settings_TextSize(40.0f);
+	CP_Settings_NoTint();
+
+	CP_Settings_Save();
 }
 
-// use CP_Engine_SetNextGameState to specify this function as the update function
-// this function will be called repeatedly every frame
-void game_update(void)
-{
-	// check input, update simulation, render etc.
+void gameInit(void) {
+	settingsInit();
+}  
+
+void gameUpdate(void) { 
+	CP_Graphics_ClearBackground(BLACK);
+
+	if (CP_Input_KeyReleased(KEY_R)) CP_Settings_Tint(RED);
+	if (CP_Input_KeyReleased(KEY_G)) CP_Settings_Tint(GREEN);
+	if (CP_Input_KeyReleased(KEY_B)) CP_Settings_Tint(BLUE);
+	if (CP_Input_KeyReleased(KEY_C)) CP_Settings_Tint(CYAN);
+	if (CP_Input_KeyReleased(KEY_Y)) CP_Settings_Tint(YELLOW);
+	if (CP_Input_KeyReleased(KEY_M)) CP_Settings_Tint(MAGENTA);
+
+	CP_Settings_Fill(GRAY);
+
+	CP_Graphics_DrawRect(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 2);
+
 }
 
-// use CP_Engine_SetNextGameState to specify this function as the exit function
-// this function will be called once just before leaving the current gamestate
-void game_exit(void)
-{
-	// shut down the gamestate and cleanup any dynamic memory
+void gameExit(void) { 
+
 }
 
-// main() the starting point for the program
-// CP_Engine_SetNextGameState() tells CProcessing which functions to use for init, update and exit
-// CP_Engine_Run() is the core function that starts the simulation
+void terminateController(void) {
+	if (CP_Input_KeyReleased(KEY_ESCAPE) || CP_Input_KeyReleased(KEY_Q)) CP_Engine_Terminate();
+}
+
 int main(void)
 {
-	CP_Engine_SetNextGameState(game_init, game_update, game_exit);
+	CP_Engine_SetPostUpdateFunction(terminateController);
+	CP_Engine_SetNextGameState(gameInit, gameUpdate, gameExit);
 	CP_Engine_Run();
 	return 0;
 }
