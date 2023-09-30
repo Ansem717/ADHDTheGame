36f758ae176714065b77143a81267cc12a48ff3f
Ansem717 <me@andymalik.com>
2023-09-30 02:40:22 -0700

    Fixed the math for the lanes. Started to implement controller but got bored.

--- lanedriver.c
+++ lanedriver.c
@@ -19,6 +19,10 @@ float lineWidth, lineHeight, lineSpacing, lineY;
 float drivingSpeed;
 int lineFrequency;
 
+CP_Vector carPos;
+float carWidth, carHeight;
+int currentLane;
+
 void laneDriverInit(Game g) {
 	lineWidth = 6; //Width of Lane Lines
 	lineHeight = 50; //Height of Lane Lines
@@ -28,15 +32,23 @@ void laneDriverInit(Game g) {
 	numberOfLanes = 4;
 	drivingSpeed = 5;
 
+
+
 	roadWidth = g.w - PADDING;
-	laneWidth = (roadWidth - 3 * lineWidth) / numberOfLanes;
+	laneWidth = roadWidth / numberOfLanes;
 	for (int i = 0; i < numberOfLanes; i++) {
-		laneXPos[i] = g.x + PADDING + (laneWidth + lineWidth) * i + laneWidth / 4;
+		laneXPos[i] = g.x + laneWidth / 2 + PADDING / 2 + laneWidth * i;
 	}
 
+	currentLane = CP_Random_RangeInt(0, numberOfLanes - 1);
+	carPos.x = laneXPos[currentLane];
+	carPos.y = g.y + g.h * 25 / 32;
+	carWidth = laneWidth * 3 / 5;
+	carHeight = 80;
 }
 
 void laneDriverPlay(Game g) {
+	carPos.x = laneXPos[currentLane];
 	
 	//Draw Road
 	stroke(getColor(BLACK), 0);
@@ -47,15 +59,32 @@ void laneDriverPlay(Game g) {
 	fill(getColor(WHITE));
 	for (int i = 0; i < numberOfLanes - 1; i++) {
 		for (int k = 0; k < lineFrequency; k++) {
-			CP_Graphics_DrawRect(laneXPos[i] + laneWidth / 2, fmod(k * (lineHeight + lineSpacing) + lineY, g.h + PADDING/2) - PADDING/2, lineWidth, lineHeight);
+			CP_Graphics_DrawRect(
+				laneXPos[i] + laneWidth/2 - lineWidth/2, 
+				fmod(k * (lineHeight + lineSpacing) + lineY, g.h + PADDING/2) - PADDING/2,
+				lineWidth, 
+				lineHeight
+			);
 		}
 	}
-
 	lineY += drivingSpeed;
+
+
+	fill(g.col);
+	stroke(getColor(BLACK), 2);
+	CP_Settings_RectMode(CP_POSITION_CENTER);
+	CP_Graphics_DrawRect(carPos.x, carPos.y, carWidth, carHeight);
+	CP_Settings_RectMode(CP_POSITION_CORNER);
 
 	//Redraw top and bottom border to cut off the scrolling elements
 	stroke(g.col, 0);
 	fill(g.col);
 	CP_Graphics_DrawRect(g.x, g.y - PADDING / 2, g.w, PADDING);
 	CP_Graphics_DrawRect(g.x, g.y + g.h - PADDING / 2, g.w, PADDING);
+
+	currentLane = CP_Random_RangeInt(0, numberOfLanes - 1);
+	/*
+
+	if (CP_Input_KeyTriggered(g.cont.keyLeft) && 1) 
+	if (CP_Input_KeyTriggered(g.cont.keyRight) && 1) */
 }
