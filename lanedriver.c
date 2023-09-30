cafa64d23ce3de24d15a8599545aa9ac7a702c61
Ansem717 <me@andymalik.com>
2023-09-30 01:56:10 -0700

    Starting work on Lane Driver with an infinite scrolling road.

--- lanedriver.c
+++ lanedriver.c
@@ -9,7 +9,53 @@
 //---------------------------------------------------------
 
 #include "lanedriver.h"
+#include "utility.h"
+#include <math.h>
 
-void laneDriverInit(Game g) {}
+int numberOfLanes;
+float laneXPos[4];
+float roadWidth, laneWidth;
+float lineWidth, lineHeight, lineSpacing, lineY;
+float drivingSpeed;
+int lineFrequency;
 
-void laneDriverPlay(Game g) {}
+void laneDriverInit(Game g) {
+	lineWidth = 6; //Width of Lane Lines
+	lineHeight = 50; //Height of Lane Lines
+	lineFrequency = 7;
+	lineSpacing = (g.h - lineFrequency * lineHeight) / lineFrequency;
+
+	numberOfLanes = 4;
+	drivingSpeed = 5;
+
+	roadWidth = g.w - PADDING;
+	laneWidth = (roadWidth - 3 * lineWidth) / numberOfLanes;
+	for (int i = 0; i < numberOfLanes; i++) {
+		laneXPos[i] = g.x + PADDING + (laneWidth + lineWidth) * i + laneWidth / 4;
+	}
+
+}
+
+void laneDriverPlay(Game g) {
+	
+	//Draw Road
+	stroke(getColor(BLACK), 0);
+	fill(CP_Color_Create(30, 30, 30, 255));
+	CP_Graphics_DrawRect(g.x + PADDING / 2, g.y + PADDING/2, roadWidth, g.h - PADDING);
+
+	//Draw White Passing Line Segments
+	fill(getColor(WHITE));
+	for (int i = 0; i < numberOfLanes - 1; i++) {
+		for (int k = 0; k < lineFrequency; k++) {
+			CP_Graphics_DrawRect(laneXPos[i] + laneWidth / 2, fmod(k * (lineHeight + lineSpacing) + lineY, g.h + PADDING/2) - PADDING/2, lineWidth, lineHeight);
+		}
+	}
+
+	lineY += drivingSpeed;
+
+	//Redraw top and bottom border to cut off the scrolling elements
+	stroke(g.col, 0);
+	fill(g.col);
+	CP_Graphics_DrawRect(g.x, g.y - PADDING / 2, g.w, PADDING);
+	CP_Graphics_DrawRect(g.x, g.y + g.h - PADDING / 2, g.w, PADDING);
+}
